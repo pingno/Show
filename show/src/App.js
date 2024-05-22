4ceba2e8adb1ce2bd012a14e4f22f2a5cd243104
peang <pngo93@gmail.com>
2024-05-21 19:52:42 -0500

    footer fixed

--- show/src/App.js
+++ show/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
 
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
