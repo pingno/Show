4959186375a6505955c3dc775efc991d9d71ebe5
peang <pngo93@gmail.com>
2024-06-04 21:17:32 -0500

    about section

--- show/src/components/About/index.js
+++ show/src/components/About/index.js
@@ -1,13 +1,33 @@
-import React from 'react';
-import './About.css';
+import React from "react";
+import "./About.css";
 
 const About = () => {
   return (
     <div className="about-container">
       <h2 className="about-title">About Me</h2>
       <p className="about-description">
-        Hi, I'm [Your Name], a passionate full stack developer with experience in building web applications using the latest technologies. I specialize in JavaScript, React, Node.js, and have a keen interest in UX/UI design. I love solving complex problems and constantly strive to learn and grow as a developer.
+        Hi, I'm Peang, a passionate full stack developer with experience in
+        building web applications using the latest technologies. I specialize in
+        JavaScript, React, Node.js, and have a keen interest in UX/UI design. I
+        love solving complex problems and constantly strive to learn and grow as
+        a developer.
       </p>
+
+      <p>
+        My values have always aligned with building a skillset where I could
+        help people, whether that's in business development, creative work, or
+        personal growth.
+      </p>
+
+      <div>My Journey</div>
+
+      <p>
+        High School - School of Business and Management at Yvonne E. Ewell
+        Townview Center
+      </p>
+      <p>College - Associates degree in Liberal Arts</p>
+
+      <p>App Academy - Full Stack Web Development </p>
     </div>
   );
 };
