9332770b31e19283956c1ef3e9320d7e664092ee
peang <pngo93@gmail.com>
2024-07-04 18:46:11 -0500

    skills app js

--- show/src/App.js
+++ show/src/App.js
@@ -9,7 +9,7 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
-// import Skills from "./components/Skills";
+import Skills from "./components/Skills";
 // import Contact from "./components/Contact";
 
 const App = () => {
@@ -27,9 +27,10 @@ const App = () => {
           {/* Add other routes here */}
           {/* <Route path="/about" element={<About />} />
           
-          <Route path="/skills" element={<Skills />} />
+          
            />
           <Route path="/contact" element={<Contact />} /> */}
+          <Route path="/skills" element={<Skills />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/experience" element={<Experience />} />
           <Route path="/contact" element={<Contact />} />
