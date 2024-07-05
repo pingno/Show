ba49b3b1dac30523682611feb883f0ed3562876d
peang <pngo93@gmail.com>
2024-07-04 19:03:54 -0500

    skilsdata page included

--- show/src/components/Skills/index.js
+++ show/src/components/Skills/index.js
@@ -1,46 +1,23 @@
-import React, { useState } from 'react';
-import './Skills.css';
+// src/components/Skills/index.js
 
-const skillsData = [
-  { id: 'skill1', name: 'JavaScript', description: 'Experienced with ES6+ syntax and modern frameworks like React.', icon: 'path/to/javascript-icon.png' },
-  { id: 'skill2', name: 'CSS', description: 'Proficient in responsive design, Flexbox, and CSS Grid.', icon: 'path/to/css-icon.png' },
-  // Add more skills as needed
-];
+import React from 'react';
+import './Skills.css';
+import skillsData from './skillsData';
 
 const Skills = () => {
-  const [selectedSkill, setSelectedSkill] = useState(null);
-
-  const openModal = (skill) => {
-    setSelectedSkill(skill);
-  };
-
-  const closeModal = () => {
-    setSelectedSkill(null);
-  };
-
   return (
     <div className="skills-container">
       <h1 className="skills-title">My Skills</h1>
       <div className="skills-grid">
         {skillsData.map(skill => (
-          <div className="skill-card" key={skill.id} onClick={() => openModal(skill)}>
+          <div className="skill-card" key={skill.id}>
             <div className="skill-icon">
               <img src={skill.icon} alt={`${skill.name} Icon`} />
             </div>
-            <h2 className="skill-title">{skill.name}</h2>
+            <h2 className="skill-name">{skill.name}</h2>
           </div>
         ))}
       </div>
-
-      {selectedSkill && (
-        <div className="modal" onClick={closeModal}>
-          <div className="modal-content" onClick={e => e.stopPropagation()}>
-            <span className="close-button" onClick={closeModal}>&times;</span>
-            <h2 className="modal-skill-title">{selectedSkill.name}</h2>
-            <p className="modal-skill-description">{selectedSkill.description}</p>
-          </div>
-        </div>
-      )}
     </div>
   );
 };
---
+++ show/src/components/Skills/skillsData.js
@@ -0,0 +1,8 @@
+const skillsData = [
+    { id: 'skill1', name: 'JavaScript', icon: 'path/to/javascript-icon.png' },
+    { id: 'skill2', name: 'Python', icon: 'path/to/python-icon.png' },
+    { id: 'skill3', name: 'SQL', icon: 'path/to/sql-icon.png' },
+    // Add more skills as needed
+  ];
+  
+  export default skillsData;
