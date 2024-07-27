39f96da411bc7fe6ba4b9b901c5222d736c9849f
peang <pngo93@gmail.com>
2024-07-27 01:42:42 -0500

    start of blog component

---
+++ show/src/components/Blog/index.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import './Blog.css';
+
+const posts = [
+  {
+    id: 1,
+    title: 'My First Blog Post',
+    date: '2024-07-27',
+    excerpt: 'This is a short summary of my first blog post...',
+    content: 'This is the full content of my first blog post...'
+  },
+  // Add more posts as needed
+];
